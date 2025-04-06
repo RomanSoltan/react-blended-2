a5b41529815de962abe858f2830ab96ab4f395f7
Roman Soltan <romabandit88@gmail.com>
2025-04-06 22:31:13 +0300

    step 1

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,8 +1,14 @@
+import Form from '../components/Form/Form';
 import Text from '../components/Text/Text';
 
 const Todos = () => {
+  const addNewTodo = inputValue => {
+    console.log(inputValue); // має вивести значення інпуту під час сабміту форми
+  };
+
   return (
     <>
+      <Form onSubmit={addNewTodo} />
       <Text textAlign="center">There are no any todos ...</Text>
     </>
   );
